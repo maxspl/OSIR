9000a08a0d4c02a8391307b9c0e84b5b32d3833a
maxspl <spellanzon.maxime@gmail.com>
2026-02-01 14:44:08 +0100

    [FIX] issues related to refactor (json2splunk)

--- OSIR/src/osir_lib/osir_lib/modules/indexer/indexer.py
+++ OSIR/src/osir_lib/osir_lib/modules/indexer/indexer.py
@@ -3,15 +3,15 @@ import os
 import copy
 from datetime import datetime
 from osir_lib.core.OsirDecorator import osir_internal_module
+from osir_lib.core.model.OsirModuleModel import OsirModuleModel
 from osir_lib.core.OsirModule import OsirModule
-from osir_lib.core.LogUtils import LogUtils
 from osir_lib.logger import AppLogger, CustomLogger
 
 logger: CustomLogger = AppLogger().get_logger()
 
 
 @osir_internal_module
-class InjectionModule(LogUtils):
+class InjectionModule():
     """
     PyModule to inject log into Splunk.
     """
@@ -21,10 +21,8 @@ class InjectionModule(LogUtils):
         Initializes the Module.
 
         Args:
-            case_path (str): The directory path where case files are stored and operations are performed.
             module (OsirModule): Instance of OsirModule containing configuration details for the extraction process.
         """
-        LogUtils.__init__(self, ctx=module)
         self.module = module
         self._dir_to_process = module.input.match
 
@@ -37,26 +35,38 @@ class InjectionModule(LogUtils):
         """
         try:
             logger.debug(f"Processing file {self._dir_to_process}")
-            for dir_name in os.listdir(self._dir_to_process):
-                if os.path.isdir(os.path.join(self._dir_to_process, dir_name)):
+            for module_dir_name in os.listdir(self._dir_to_process):
+                if os.path.isdir(os.path.join(self._dir_to_process, module_dir_name)):
                     try:
-                        module_to_process: OsirModule = OsirModule.from_name(dir_name)
+                        child_module_model = OsirModuleModel.from_name(module_dir_name)
                     except FileNotFoundError:
-                        logger.warning(f"Skipping directory '{dir_name}' — no matching module found.")
+                        logger.warning(f"Skipping directory '{module_dir_name}' — no matching module found.")
                         continue  # Skip to next directory
-                    if module_to_process:
-                        logger.debug(f"Module found {dir_name}")
-                        indexer_data = module_to_process.splunk
-                        if indexer_data:
-                            tool_ = copy.deepcopy(self.module.tool)
-                            tool_.cmd = tool_.cmd.replace('{indexer_path}', os.path.join(self._dir_to_process, module_to_process._module_filepath))
-                            tool_.cmd = tool_.cmd.replace('{input_dir}', os.path.join(self._dir_to_process, dir_name))
-                            tool_.cmd = tool_.cmd.replace('{case_name}', os.path.basename(self._dir_to_process))
-                            tool_.run_local()
+                    if child_module_model:
+                        logger.debug(f"Module found {module_dir_name}")
+
+                        # Dump model to add mandatory fields before validation : case_path and match
+                        child_module_data = child_module_model.model_dump()
+                        child_module_data["case_path"] = self.module.case_path
+                        child_module_data["input"]["match"] = os.path.join(self._dir_to_process, module_dir_name)
+
+                        # Transform model to instance. Mandatory to get _module_filepath
+                        child_module_instance = OsirModule.model_validate(child_module_data)
+
+                        replacements = {
+                            "indexer_path": child_module_instance._module_filepath
+                        }
+                        self.module.tool.cmd = self.module.tool.safe_format(self.module.tool.cmd, **replacements)
+
+                        # Run json2splunk is splunk configuration exists
+                        splunk_indexer_data = child_module_instance.splunk
+                        if splunk_indexer_data:
+                            self.module.tool.run()
                     else:
-                        logger.warning(f"Module not found {dir_name}")
+                        logger.warning(f"Module not found {module_dir_name}")
 
             logger.debug(f"{self.module.module_name} done")
 
         except Exception as exc:
             logger.error_handler(exc)
+
